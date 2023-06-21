dbc6bafec670a26efc87efdb40380dddd3a2d828
HagenKH <94391048+HagenKH@users.noreply.github.com>
2023-06-21 08:19:35 -0400

    BOOMS PGD RESULTS

--- demo/demo_problem_solver.py
+++ demo/demo_problem_solver.py
@@ -29,7 +29,7 @@ from simopt.experiment_base import ProblemSolver, read_experiment_results, post_
 
 # Example with random search solver on continuous newsvendor problem.
 # -----------------------------------------------
-solver_name = "BOOM-FW"  # Random search solver
+solver_name = "BOOM-PGD"  # Random search solver
 problem_name = "OPENJACKSON-1"  # Continuous newsvendor problem
 # -----------------------------------------------
 
